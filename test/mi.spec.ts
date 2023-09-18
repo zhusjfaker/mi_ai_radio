10a615d05727f2f08c1e25f896a1b58ff1c64d3c
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2023-09-18 22:22:22 +0800

    feat: support mi account login

--- test/mi.spec.ts
+++ test/mi.spec.ts
@@ -79,9 +79,13 @@ describe('MI OPEN API', () => {
       60000
     );
 
-    test.skip('test get last ask content', async () => {
-      const result = await ai.get_radio_last_ask();
-      console.log(JSON.stringify(result, null, 2));
-    }, 60000);
+    test.concurrent(
+      'test get last ask content',
+      async () => {
+        const result = await ai.get_radio_last_ask();
+        console.log(JSON.stringify(result, null, 2));
+      },
+      60000
+    );
   }
 });
